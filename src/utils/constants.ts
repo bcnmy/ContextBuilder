d1514018efa2d41944e591bad6afecb7d139cc85
GabiDev <gv@popoo.io>
2024-08-02 13:32:30 +0300

    feat: updated smart session address

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,7 +5,7 @@ export const uniActionPolicyAddress = "0x960a562fa790973FF6BB3493De4322499711E95
 export const multiKeySignerAddress = "0xdB3CCF893b55020153444e163EB0e7fCB4F2f721"; 
 export const simpleSignerAddress = "0xA4eA81B60E6Aea3999E1C4eE335A23463e1c4DFb";
 
-export const smartSessionAddress = "0x199F72ce654d3bE1bdfc10BE4Eda7eabbF31909b";
+export const smartSessionAddress = "0x006F777185cf3F0B152E8CEE93587395Aee15129";
 export const userOperationBuilderAddress = "0xeD14b1D3a5ee527E2d6B63dd0e6681090B770F0F";
 export const mockValidator = "0x9C08e1CE188C29bAaeBc64A08cF2Ec44207749B6";
 export const counterContractAddress = "0x780325C200726176AE17De6a778A582e5adf1F15"; 

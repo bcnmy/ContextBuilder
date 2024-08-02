851a402177e7d723eb719900a6478059a6ef60e8
GabiDev <gv@popoo.io>
2024-08-02 22:22:37 +0300

    refactor: update mock validator address and bump v

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,7 +7,7 @@ export const simpleSignerAddress = "0xA4eA81B60E6Aea3999E1C4eE335A23463e1c4DFb";
 
 export const smartSessionAddress = "0x006F777185cf3F0B152E8CEE93587395Aee15129";
 export const userOperationBuilderAddress = "0xeD14b1D3a5ee527E2d6B63dd0e6681090B770F0F";
-export const mockValidator = "0x9C08e1CE188C29bAaeBc64A08cF2Ec44207749B6";
+export const mockValidator = "0xaE15a31afb2770cE4c5C6131925564B03b597Fe3";
 export const counterContractAddress = "0x780325C200726176AE17De6a778A582e5adf1F15"; 
 export const digestEncoderAddress = "0x61467D2e2656CacC8eF3C975A865770963B4aBcD"; 
 

7f9c161cc8408ac3b3bac3c00457c94e1dc0d9d9
VGabriel45 <vgabrielmarian21@gmai.com>
2024-08-30 11:12:05 +0300

    feat: updated contract addresses for base sepolia and bumped version

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,13 +1,8 @@
 // POLICIES
-// export const valueLimitPolicyAddress = "0x09EBE803cda891B4C3c3F92A3875a0d9f16e9329"; // value-limit
-export const timeFramePolicyAddress = "0x2A0F4538e1D5341638BBba1578681d4D1622338e"; // time-frame 
-export const uniActionPolicyAddress = "0x960a562fa790973FF6BB3493De4322499711E956"; // uni-action 
-export const multiKeySignerAddress = "0xdB3CCF893b55020153444e163EB0e7fCB4F2f721"; 
-export const simpleSignerAddress = "0xA4eA81B60E6Aea3999E1C4eE335A23463e1c4DFb";
+export const timeFramePolicyAddress = "0x0B7BB9bD65858593D97f12001FaDa94828307805";
+export const multiKeySignerAddress = "0x24084171C36Fa6dfdf41D9C89A51F600ed35A731";
 
-export const smartSessionAddress = "0x006F777185cf3F0B152E8CEE93587395Aee15129";
-export const userOperationBuilderAddress = "0xeD14b1D3a5ee527E2d6B63dd0e6681090B770F0F";
-export const mockValidator = "0xaE15a31afb2770cE4c5C6131925564B03b597Fe3";
-export const counterContractAddress = "0x780325C200726176AE17De6a778A582e5adf1F15"; 
-export const digestEncoderAddress = "0x61467D2e2656CacC8eF3C975A865770963B4aBcD"; 
+export const smartSessionAddress = "0x0ce452fD9d0a20BCC14A2C4830E22bEB7EA4C8ee";
+export const userOperationBuilderAddress = "0xb07D7605a1AAeE4e56915363418229c127fF7C3D";
+export const mockValidator = "0x61Cb30337CB980383704f2Debfb8bea66d0f26b3";
 

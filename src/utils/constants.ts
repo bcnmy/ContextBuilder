21267bd9fd126ac6c97e697d0154d150812c7426
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2024-09-05 23:03:58 +0400

    build fixes

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,24 +1,24 @@
-export const timeFramePolicyAddress = {
+export const timeFramePolicyAddress: { [key: number]: `0x${string}` } = {
     84532: "0x0B7BB9bD65858593D97f12001FaDa94828307805",
     11155111: "0x2A0F4538e1D5341638BBba1578681d4D1622338e"
 }
 
-export const multiKeySignerAddress = {
+export const multiKeySignerAddress: { [key: number]: `0x${string}` } = {
     84532: "0x24084171C36Fa6dfdf41D9C89A51F600ed35A731",
     11155111: "0x3cA2D7D588FA66248a49c1C885997e5017aF9Dc7"
 }
 
-export const smartSessionAddress = {
+export const smartSessionAddress: { [key: number]: `0x${string}` } = {
     84532: "0x0ce452fD9d0a20BCC14A2C4830E22bEB7EA4C8ee",
     11155111: "0x39E213e4e1b0256563B7d8029De669ff0eBE1237"
 }
 
-export const userOperationBuilderAddress = {
+export const userOperationBuilderAddress: { [key: number]: `0x${string}` } = {
     84532: "0xb07D7605a1AAeE4e56915363418229c127fF7C3D",
     11155111: "0xeD14b1D3a5ee527E2d6B63dd0e6681090B770F0F"
 }
 
-export const mockValidator = {
+export const mockValidator: { [key: number]: `0x${string}` } = {
     84532: "0x61Cb30337CB980383704f2Debfb8bea66d0f26b3",
     11155111: "0xaE15a31afb2770cE4c5C6131925564B03b597Fe3"
 }
